ae5fc2c022e54dad61d800f272e4d5e4347863ab
qvarkk <xumdes@gmail.com>
2022-07-21 18:19:18 +0500

    reformed code to fit change size button

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-let gridSize = 20;
-let tileSize = (720 / gridSize) - 2;
-
+// Storing css variables in rs variable
 const r = document.querySelector(':root');
 const rs = getComputedStyle(r);
 
-r.style.setProperty('--size', tileSize.toString() + 'px');
-// alert(rs.getPropertyValue('--size'));
-
-const canvas = document.getElementById('canvas');
-
+// Creates tile node and adds classes to it
 const tile = document.createElement('div');
 tile.classList.add('tile', 'tile-borders');
-console.log(tile.classList);
 
-// canvas.appendChild(tile);
-for (let i = 0; i < gridSize * gridSize; i++) {
-    canvas.appendChild(tile.cloneNode(true));
+// Loops for grid's area times and creates a tile on every step
+let tileSize;
+let allTiles;
+const canvas = document.getElementById('canvas');
+let createNewCanvas  = (gridSize) => {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        tileSize = (720 / gridSize) - 2;
+        canvas.appendChild(tile.cloneNode(true));
+        r.style.setProperty('--size', tileSize.toString() + 'px');
+    }
+
+    // Creating a node list of all tiles to make them responsible on click
+    allTiles = document.querySelectorAll('.tile');
+    allTiles.forEach((tile) => {
+        tile.addEventListener('click', () => {
+            tile.style.backgroundColor = 'black';
+        })
+        tile.addEventListener('dblclick', () => {
+            tile.style.backgroundColor = 'white';
+        })
+    });
 }
+// To create canvas on page load
+createNewCanvas(16);
 
-console.log(`grid size = ${gridSize}`);
-console.log(`tileSize = ${tileSize}`);
-console.log(`${rs.getPropertyValue('--size')}`);
+
+
+// If input after clicking "change size" button is wrong, it won't create new canvas
+let checkForInput = (inputSize) => {
+    if (5 <= inputSize && inputSize <= 100) {
+        allTiles = document.querySelectorAll('.tile');
+        allTiles.forEach((tile) => {
+            canvas.removeChild(tile);
+        });
+        createNewCanvas(inputSize);
+    } else if (inputSize === null) { // in case if user tries to close the prompt
+        return;
+    } else {
+        console.log(inputSize);
+        alert('Wrong input value, try again!');
+        inputSize = prompt('Enter new size you want to see (min 5, max 100)');
+        checkForInput(inputSize);
+    }
+};
+
+// 
+let inputedSize;
+const sizeButton = document.getElementById('change-size');
+sizeButton.addEventListener('click', () => {
+    inputedSize = prompt('Enter new size you want to see (min 10, max 100)');
+    checkForInput(inputedSize);
+});
